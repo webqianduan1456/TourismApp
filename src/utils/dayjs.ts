7394bbcd39859068156407a98b050e3188cd8339
无间之水 <1989315788@qq.com>
2025-04-14 11:14:21 +0800

    4/14

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,10 +1,10 @@
 import dayjs from "dayjs";
 
 // 日期格式化
-export function utilsDate(data) {
+export function utilsDate(data: string | number | Date | dayjs.Dayjs | null | undefined) {
   return dayjs(data).format('MM月DD日')
 }
 // 计算相差的天数
-export function StayCount(beforeDate, behindDate){
+export function StayCount(beforeDate:string, behindDate:string){
   return dayjs(behindDate).diff(beforeDate, "day")
 }
